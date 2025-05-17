9d4b25993e98eb870d64c3edd4e458f64b68c162
Acer-code <infoacerbiomed@gmail.com>
2025-05-17 17:48:07 +0530

    settings.py

--- acersite/settings.py
+++ acersite/settings.py
@@ -25,11 +25,12 @@ import cloudinary.api
 
 pymysql.install_as_MySQLdb()
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+# load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
